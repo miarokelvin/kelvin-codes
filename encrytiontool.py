050d9148746550ab6316ec5f88a5be09cdccb1c5
miarokelvin <miarokelvin@gmail.com>
2026-01-15 13:00:07 +0300

    intial commit

--- encrytiontool.py
+++ encrytiontool.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-chars= " " + string.punctuation + string.digits + string.ascii_letters
+chars= " " + string.punctuation + string.dis + string.ascii_letters
 chars=list(chars)
 key=chars.copy()
 random.shuffle(key)
